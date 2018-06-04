860f55d52622bfa6038b044910b75c761bbbb6f3
rohanp08 <pawarrohan98@gmail.com>
2018-06-04 14:28:05 +0530

    date and time updated

--- src/pages/page-gmap-autocomplete/page-gmap-autocomplete.ts
+++ src/pages/page-gmap-autocomplete/page-gmap-autocomplete.ts
@@ -145,6 +145,7 @@ export class PageGmapAutocomplete implements OnInit {
  ngOnInit() {
         this.initMap();
         this.initPlacedetails();
+        console.log("CURRENT DATE: "+this.myDate);
     }
 
  showModal() {
