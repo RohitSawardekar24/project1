b395e89e9bf608213dfafe4490472835b65f5e90
rohanp08 <pawarrohan98@gmail.com>
2018-06-05 11:49:45 +0530

    profile pic

--- src/pages/profile-pic/profile-pic.ts
+++ src/pages/profile-pic/profile-pic.ts
@@ -1,82 +1,85 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController, LoadingController, Platform } from 'ionic-angular';
-import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { NavController, NavParams, AlertController, LoadingController, Platform, Events } from 'ionic-angular';
+import { FileChooser } from '@ionic-native/file-chooser';
 import { FilePath } from '@ionic-native/file-path';
-import { FileChooser } from '@ionic-native/file-chooser'
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { NgZone } from '@angular/core';
 import { Storage } from '@ionic/storage';
-import { Toast } from '@ionic-native/toast'
 import { GoogleAnalytics } from '@ionic-native/google-analytics';
-import { Diagnostic } from '@ionic-native/diagnostic';
-import { MyApp } from '../../app/app.component';
 import { NetworkServiceProvider } from '../../providers/network-service/network-service';
 @Component({
   selector: 'page-profile-pic',
   templateUrl: 'profile-pic.html'
 })
 export class ProfilePicPage {
+ionViewDidEnter(){
+     this.loadData()
+    }
   profilePicForm:any;
   items:any;
+  options:any;
   progress: number;
   completed:boolean;
   http:any;
   id:any;
   hash:any;
-  drive_name:any;
   social_pic:any;
-  constructor(public transfer: FileTransfer,
-              private fp: FilePath ,
-              public ga: GoogleAnalytics,
-              private fc:FileChooser,
-              public network: NetworkServiceProvider,
-              private loadingCtrl: LoadingController,
+  drive_name:any;
+  constructor(private loadingCtrl: LoadingController, 
               http: Http, 
-              //public myapp: MyApp,
+              public network: NetworkServiceProvider,
+              private filePath: FilePath,
+              private filetransfer: FileTransfer,
+              private filechooser: FileChooser,
               private storage: Storage, 
               private ngZone: NgZone, 
               private platform: Platform, 
               public navCtrl: NavController, 
               public navParams: NavParams, 
               private alertCtrl: AlertController, 
-              private toastCtrl:Toast,
-              public diagnostic: Diagnostic) {
-           if(this.network.noConnection()){
-              this.network.showNetworkAlert()
-            }else{
-              let loader = this.loadingCtrl.create({
-                  spinner: "bubbles",
-                  content: "Fetching your Account Details. Kindly wait...",
-                });
-              
-              this.social_pic = false
-              loader.present();
-              this.storage.get("id").then((id) =>{
-                  this.platform.ready().then(() => {
-                  this.ga.trackEvent("Update Profile Picture", "Opened", "New Session Started", id, true)
-                  this.ga.setAllowIDFACollection(true)
-                  this.ga.setUserId(id)
-                  this.ga.trackView("Update Profile Picture")
+              private ga: GoogleAnalytics,
+              public events: Events
+              ) {           
+            this.http = http;    
+    }
+      loadData(){
+      if(this.network.noConnection()){
+        this.network.showNetworkAlert()
+    }
+      else{  
+      let loader = this.loadingCtrl.create({
+      content: "Fetching your Account Details. Kindly wait...",
+    });
+        
+        this.social_pic = false
+        loader.present();
+        this.storage.get('user').then((id) =>{
+                this.ga.trackEvent("Update Profile Pic Page", "Opened", "", id.id)
+                this.ga.trackView("Update Profile Pic")
               });
-                    this.http = http;
-                    var url="http://www.forehotels.com:3000/api/package/"+id;
-                    this.getDetails(url, loader);
-          });
-        }             
-  }
+        this.storage.get('Hash').then((hash) => {
+            this.hash = hash;
+        });
+        this.storage.get('id').then((id) =>{
+              
+              var url="http://www.forehotels.com:3000/api/package/"+id;
+              this.getDetails(url, loader);
+              this.id = id;
+            });  
+          }
+      }
   getDetails(x, loader){
-    this.storage.get("Hash").then((value)=>{
      let headers = new Headers({
       'Content-Type': 'application/json',
-      'Authorization': value
+      'Authorization': this.hash
     });
     let options = new RequestOptions({ headers: headers });
             this.http.get(x, options)
             .subscribe(data =>{
              this.items=JSON.parse(data._body).Jobs;
-            let split_name = (this.items["0"].name).split(" ")
-              this.drive_name = split_name[0]
-             let img = this.items["0"].profile_pic.split("/")
+             let img = this.items[0].profile_pic.split("/")
+             this.drive_name = this.items["0"].email.split('@')
              if(img.length > 1){
                this.social_pic = true;
              }
@@ -84,15 +87,14 @@ export class ProfilePicPage {
             },error=>{
                 console.log(error);
             } );
-          }) 
    }
   findProfilePic(){
-    this.fc.open()
+    this.filechooser.open()
       .then(
         uri => {
           let DrivePicpath = uri.split("/") 
           if(DrivePicpath[0] == 'content:'){
-              let fileTransfer: FileTransferObject = this.transfer.create();
+              let fileTransfer: FileTransferObject = this.filetransfer.create();
                       fileTransfer.download(uri, "file:///storage/emulated/0/Download/" +this.drive_name[0]+'.jpg').then((entry) => {                        
                         let tourl = entry.toURL()
                         this.profilePicUpload(tourl)
@@ -100,7 +102,7 @@ export class ProfilePicPage {
                         // handle error
                       });
           }else{
-            this.fp.resolveNativePath(uri)
+            this.filePath.resolveNativePath(uri)
             .then(filePath => {                 
               this.profilePicUpload(filePath);
             });
@@ -110,71 +112,68 @@ export class ProfilePicPage {
   
   profilePicUpload(x){
     if(this.network.noConnection()){
-       this.network.showNetworkAlert()
-      }else{ 
-            this.completed=false;
-            var fileArray = x.split("/");
-            let len = fileArray.length;
-            let file = fileArray[len - 1];
-            var filebits = file.split(".");
-            var f = filebits[1];
+        this.network.showNetworkAlert()
+    }
+      else{
+      this.completed=false;
+      var fileArray = x.split("/");
+      let len = fileArray.length;
+      let file = fileArray[len - 1];
+      var filebits = file.split(".");
+      var f = filebits[1];
 
-            if((f != "jpg") && (f != "png") && (f != "jpeg")){
-              let alert = this.alertCtrl.create({
-                    title: "Invalid File Format",
-                    subTitle: "Allowed File extensions are JPG, PNG, and JPEG only",
-                    buttons: ['Dismiss'],
-                  });
-                  alert.present();
-            }else{
-            let fileTransfer: FileTransferObject = this.transfer.create();            
-            this.storage.get("id").then((id)=> {
-              this.storage.get("Hash").then((hash)=>{
-              let options: FileUploadOptions ={
-                  fileKey: 'img',
-                  fileName: x,
-                  mimeType: "multipart/form-data",
-                  headers: {
-                    authorization : hash
-                  },
-                  params: {
-                    name: file,
-                    id: id
-                  }
-              }
-                let onProgress =  (progressEvent: ProgressEvent) : void => {
-                  this.ngZone.run(() => {
-                      if (progressEvent.lengthComputable) {
-                          let progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
-                          this.progress = progress    
-                      }
-                  });
-              }
-                this.completed = false;
-                fileTransfer.onProgress(onProgress)
-                fileTransfer.upload(x, encodeURI("http://www.forehotels.com:3000/api/upload_hotel_image"), options, true)
-                .then((data) => {
-                  this.progress=null;
-                  this.completed=true;
-                  let loader = this.loadingCtrl.create({
-                    spinner: "bubbles",
-                    content: "Fetching your Account Details. Kindly wait...",
-                  });
-                  loader.present();
-                  let url="http://www.forehotels.com:3000/api/package/"+id;
-                  this.getDetails(url, loader);
-                  //this.myapp.getDetails(this.id)
-                }, (err) => {
-                  let alert = this.alertCtrl.create({
-                        title: err.text(),
-                        subTitle: err.json(),
-                        buttons: ['Dismiss'],
-                      });
-                      alert.present();
-              });
-            })
-          })
+      if((f != "jpg") && (f != "png") && (f != "jpeg")){
+        let alert = this.alertCtrl.create({
+              title: "Invalid File Format",
+              subTitle: "Allowed File extensions are JPG, JPEG and PNG only",
+              buttons: ['Dismiss'],
+            });
+            alert.present();
+      }
+      else{
+        let fileTransfer: FileTransferObject = this.filetransfer.create();
+      this.options = {
+        fileKey: 'img',
+        fileName: x,
+        mimeType: "multipart/form-data",
+        headers: {
+          authorization : 'e36051cb8ca82ee0Lolzippu123456*='
+        },
+        params: {
+          name: file,
+          id: this.id
         }
       }
-  }  
+      
+       let onProgress =  (progressEvent: ProgressEvent) : void => {
+        this.ngZone.run(() => {
+            if (progressEvent.lengthComputable) {
+                let progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+                this.progress = progress    
+            }
+        });
+    }
+      this.completed = false;
+      fileTransfer.onProgress(onProgress)
+      fileTransfer.upload(x, encodeURI("http://www.forehotels.com:3000/api/upload_hotel_image"), this.options, true)
+      .then((data) => {
+        this.progress=null;
+        this.completed=true;
+        let loader = this.loadingCtrl.create({
+          content: "Fetching your Account Details. Kindly wait...",
+        });
+        loader.present();
+        let url="http://www.forehotels.com:3000/api/package/"+this.id;
+        this.getDetails(url, loader);
+      }, (err) => {
+        let alert = this.alertCtrl.create({
+              title: err.text(),
+              subTitle: err.json(),
+              buttons: ['Dismiss'],
+            });
+            alert.present();
+      });
+      }
+    }
+  }
 }
